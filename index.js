0042c4d264a3e955f8382ff4205bb4798f8ffb41
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2018-11-27 17:11:45 -0200

    some startup config

---
+++ index.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const electron = require('electron')
+
+const { app, BrowserWindow, Tray } = electron
+
+let mainWindow
+let tray
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false,
+        show: false
+    })
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`)
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
+
+    tray = new Tray(iconPath)
+
+    tray.on('click', (event, bounds) => {
+        const { x, y } = bounds
+
+        const { height, width } = mainWindow.getBounds()
+
+        if (mainWindow.isVisible()) {
+            mainWindow.hide()
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height,
+                width
+            })
+            mainWindow.show()
+        }
+    })
+})
