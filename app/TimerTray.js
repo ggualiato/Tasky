a50545f7206bb9f2ca8f807ac2d7c8e667b6ca2b
Giovanni Gualiato <ggualiato@gmail.com>
2018-12-02 02:26:11 -0200

    Refactoring for use OOP

---
+++ app/TimerTray.js
@@ -0,0 +1,34 @@
+const electron = require('electron')
+
+const { Tray } = electron
+
+class TimerTray extends Tray {
+    constructor(iconPath, mainWindow) {
+        super(iconPath)
+
+        this.mainWindow = mainWindow
+
+        this.on('click', this.onClick.bind(this))
+    }
+
+    onClick(event, bounds) {
+        const { x, y } = bounds
+
+        const { height, width } = this.mainWindow.getBounds()
+
+        if (this.mainWindow.isVisible()) {
+            this.mainWindow.hide()
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height
+            this.mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                height,
+                width
+            })
+            this.mainWindow.show()
+        }
+    }
+}
+
+module.exports = TimerTray
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const electron = require('electron')
+const TimerTray = require('./app/TimerTray')
 
 const { app, BrowserWindow, Tray } = electron
 
 let mainWindow
-let tray
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
@@ -19,24 +19,5 @@ app.on('ready', () => {
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
 
-    tray = new Tray(iconPath)
-
-    tray.on('click', (event, bounds) => {
-        const { x, y } = bounds
-
-        const { height, width } = mainWindow.getBounds()
-
-        if (mainWindow.isVisible()) {
-            mainWindow.hide()
-        } else {
-            const yPosition = process.platform === 'darwin' ? y : y - height
-            mainWindow.setBounds({
-                x: x - width / 2,
-                y: yPosition,
-                height,
-                width
-            })
-            mainWindow.show()
-        }
-    })
+    new TimerTray(iconPath, mainWindow)
 })
